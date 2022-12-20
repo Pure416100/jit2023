b2ea22a03325ea5949076046e3d7f90bcedc191c
Pure416100 <pakawat30807.6@gmail.com>
2022-12-20 20:54:53 +0700

    20/12/2065

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('googel_id')->unique()->nullable();
+            $table->string('facebook_id')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
